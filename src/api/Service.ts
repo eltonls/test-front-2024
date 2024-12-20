160c23068b8a1409a48ab2e086b24c6cbe85d004
Elton <eltonls@proton.me>
2024-12-20 20:35:06 -0300

    (feature) add basic axios config and reverse proxy

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,8 +1,16 @@
-import axios from "axios";
+import { AxiosInstance } from "axios";
+import api from "./config/axios";
 
 
 export class Service {
+  static api: AxiosInstance = api("/api");
+
   static async getUsuarios() {
+    const res = await this.api.get("/usuarios");
+  
+    console.log(res);
+  
+    return res.data;
   }
 
   static async createUsuario(usuario: any) {
