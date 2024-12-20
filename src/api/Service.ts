bc847d158e243051359a19f6bfb14ea56ed3ca33
Ana Catarina <48092743+anacatarina-l@users.noreply.github.com>
2024-12-20 17:24:47 -0300

    primeiro commit

---
+++ src/api/Service.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+export class Service {
+  static async getUsuarios() {
+  }
+
+  static async createUsuario(usuario: any) {
+  }
+
+  static async updateUsuario(id: string, dados: any) {
+  }
+
+  static async deleteUsuario(id: string) {
+  }
+}
